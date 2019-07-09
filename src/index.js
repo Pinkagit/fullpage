c85ce01115905cca9b2ffb75c9c8e2c2c73e782b
unknown <wangwuquan@Yostar.com>
2019-07-09 13:22:31 +0800

    feat: css抽离 && 页面添加背景定位偏移

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ class FullPage {
     constructor(options) {
         const defaultOptions = {        // 默认配置
             containerClassName: ".fullpage-container",
-            pageClassName: ".fullpage"
+            pageClassName: ".fullpage",
+            delay: 1000,
         }
         this.options = Object.assign(defaultOptions, options)      // 合并自定义配置
-        this.activeIndex = 1                                                                    // 当前展示的页面序号
+        this.activeIndex = 1                                                                   // 当前展示的页面序号
         this.containerDom = document.querySelector(`${this.options.containerClassName}`)        // 容器dom
         this.pagesNum = document.querySelectorAll(`${this.options.containerClassName}>${this.options.pageClassName}`).length       // 计算page页数
         this.viewHeight = document.documentElement.clientHeight                                 // 计算浏览器可视区域高度
+        this.delay = this.options.delay     // 截流和防抖函数的延迟时间
     }
     // 原型方法
     debounce(method, context, delay) {       // 防抖动函数，method 回调函数， context 上下文, delay 延迟时间
@@ -29,7 +31,7 @@ class FullPage {
                 wait = true
                 setTimeout(() => {
                     wait = false
-                }, delay)
+                }, delay)               // TODO: 截流延迟时间要大于containerDom的transition-duration + transition-delay
             }
         }
     }
@@ -51,54 +53,80 @@ class FullPage {
             this.goUp()
         }
     }
-    touchStart(event) {
-        this.startY = event.touches[0].pageY
-    }
     touchEnd(event) {
         this.endY = event.changedTouches[0].pageY
         if (this.endY - this.startY < 0) {      // 手指向上滑动，对应页面向下滚动
             this.goDown()
-        } else {                                // 手指向下滑动，对应页面向上滚动
+        } else if(this.endY - this.startY > 0) {        // 手指向下滑动，对应页面向上滚动
             this.goUp()
         }
     }
     goDown() {
         if (-this.containerDom.offsetTop <= this.viewHeight * (this.pagesNum - 2)) {
-            this.containerDom.style.top = this.containerDom.offsetTop - this.viewHeight + "px"
             this.activeIndex += 1
+            this.containerDom.style.top = this.containerDom.offsetTop - this.viewHeight + "px"
+            this.ofsetBgPosition()
         }
     }
     goUp() {
         if (-this.containerDom.offsetTop >= this.viewHeight) {
-            this.containerDom.style.top = this.containerDom.offsetTop + this.viewHeight + "px"
             this.activeIndex -= 1
+            this.containerDom.style.top = this.containerDom.offsetTop + this.viewHeight + "px"
+            this.ofsetBgPosition()
         }
     }
     resizeEvent() {
         this.viewHeight = document.documentElement.clientHeight;
         this.containerDom.style.height = this.viewHeight + "px"
         this.containerDom.style.top = - this.viewHeight * (this.activeIndex - 1) + 'px'
+        this.ofsetBgPosition()
     }
     addScrollMouseEvent() {
         if (navigator.userAgent.toLowerCase().indexOf("firefox") === -1) {      // 鼠标滚轮监听，火狐鼠标滚动事件不同其他
-            document.addEventListener("mousewheel", this.throttle(this.scrollMouse, this, 500))
+            document.addEventListener("mousewheel", this.throttle(this.scrollMouse, this, this.delay))
         } else {
-            document.addEventListener("DOMMouseScroll", this.throttle(this.scrollMouse, this, 500))
+            document.addEventListener("DOMMouseScroll", this.throttle(this.scrollMouse, this, this.delay))
         }
     }
     addTouchMoveEvent() {
-        document.addEventListener("touchstart", this.throttle(this.touchStart, this, 500))
-        document.addEventListener("touchend", this.throttle(this.touchEnd, this, 500))
+        document.addEventListener("touchstart", (event) => {
+            this.startY = event.touches[0].pageY
+        })
+        document.addEventListener("touchend", this.throttle(this.touchEnd, this, this.delay))
         document.addEventListener("touchmove", event => {        // 阻止 touchmove 下拉刷新
             event.preventDefault()
         }, { passive: false })
     }
     addResizeEvent() {
-        window.addEventListener("resize", this.debounce(this.resizeEvent, this, 500))       // 浏览器窗口大小改变时
+        window.addEventListener("resize", this.debounce(this.resizeEvent, this, this.delay))       // 浏览器窗口大小改变时
+    }
+    ofsetBgPosition() {      // 给页面添加背景定位偏移
+        for(let i = 0, len = this.containerDom.children.length; i < len; i++) {
+            let pageDom = this.containerDom.children[i];
+            if(this.activeIndex-1 === i) {
+                pageDom.style.backgroundPosition = `center center`
+            } else {
+                pageDom.style.backgroundPosition = `center ${ this.viewHeight/10*(i-(this.activeIndex-1)) }px`
+            }
+            // 设置初始样式
+            pageDom.style.height = "100%"
+            pageDom.style.backgroundAttachment = "fixed"    // 视差设置
+            pageDom.style.transition = "all 0.8s ease-in-out"
+            pageDom.style.transitionDelay = "0.2s";
+        }
+    }
+    addStyle() {        // 添加初始样式
+        document.body.style.overflowY = 'hidden';       // 隐藏body滚动条
+        this.containerDom.style.position = 'relative';
+        this.containerDom.style.height = this.viewHeight + "px"     // 初始给容器高度
+        this.containerDom.style.top = '0px';
+        this.containerDom.style.transition = "all 0.8s ease-in-out";
+        this.containerDom.style.transitionDelay = "0.2s"; 
     }
     // 初始化函数
     init() {
-        this.containerDom.style.height = this.viewHeight + "px"     // 初始给容器高度
+        this.addStyle()
+        this.ofsetBgPosition()
         this.addScrollMouseEvent()
         this.addTouchMoveEvent()
         this.addResizeEvent()
